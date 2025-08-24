8e1801c9aed45f526887aea113860f221439d415
Boitumelo <bcrachoshi@gmail.com>
2025-08-24 02:09:19 +0200

    user input

---
+++ week1/interpreter/interpreter.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+
+"""
+A program that 
+--> prompts the user for an arithmetic expression
+--> calculates the result 
+--> outputs as a floating-point value
+"""
+
+user_expression=input("Expression: ")
+
+print(user_expression)
