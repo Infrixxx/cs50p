0cc69e7c2e3d97ccc4cf0250956332752dc452a8
Boitumelo <bcrachoshi@gmail.com>
2025-08-24 05:14:36 +0200

    update to code: /

---
+++ week1/meal/meal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+program that prompts the user for a time
+---> outputs whether:
+        ---->it’s breakfast time, (7-8 am)
+        ---->lunch time, ( 12:00 - 13:00 pm)
+        ---->or dinner time. (18:00 - 19:00i) 
+"""
+
+time=input("What time is it")
