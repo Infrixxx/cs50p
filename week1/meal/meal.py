a0325a28bb53a3b8a058ff1e3f7984f8ef9174bc
Boitumelo <bcrachoshi@gmail.com>
2025-08-26 19:39:52 +0200

    fix to structure

--- week1/meal/meal.py
+++ week1/meal/meal.py
@@ -11,6 +11,7 @@ conditions:
             has to be in between a range.
 
 """
+def main():
 
 time=input("What time is it? ")     #input is always string so keep in mind to cast it to intended type.
 
@@ -33,3 +34,7 @@ elif final_time>=12 and final_time<=13:
 
 elif final_time>=18 and final_time<=19:
         print("dinner time")
+
+
+if __name__ == "__main__":
+    main()
