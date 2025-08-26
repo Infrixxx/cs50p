5c7996b4bfb8595644d94ba2c711eb38a277d24e
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-08-26 19:42:11 +0200

    Indentation fix

--- week1/meal/meal.py
+++ week1/meal/meal.py
@@ -13,18 +13,18 @@ conditions:
 """
 def main():
 
-time=input("What time is it? ")     #input is always string so keep in mind to cast it to intended type.
-
-final_time=convert(time)
-
-if final_time>=7 and final_time<=8:
-        print("breakfast time")
-
-elif final_time>=12 and final_time<=13:
-        print("lunch time")
-
-elif final_time>=18 and final_time<=19:
-        print("dinner time")
+        time=input("What time is it? ")     #input is always string so keep in mind to cast it to intended type.
+        
+        final_time=convert(time)
+        
+        if final_time>=7 and final_time<=8:
+                print("breakfast time")
+        
+        elif final_time>=12 and final_time<=13:
+                print("lunch time")
+        
+        elif final_time>=18 and final_time<=19:
+                print("dinner time")
 
 
 def convert(time):
