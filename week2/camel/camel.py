5c444d790eb8f6bcf82b553dcba211795f2a7f81
Boitumelo <bcrachoshi@gmail.com>
2025-08-27 17:22:31 +0200

    camel

---
+++ week2/camel/camel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+input --- > user inputs word in camel case
+output ---> return word in snake case
+
+camel case : first word is small letters, following words start with a capital letter
+
+snake case : words are seperated by underscores("_")
+
+"""
+
+def main()
+
+
+def snake()
+
+
+if __name__==__main__:
+    main()
