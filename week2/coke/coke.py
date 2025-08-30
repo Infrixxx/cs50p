a08074acf7ef87932583e373de073225038d5eff
Boitumelo <bcrachoshi@gmail.com>
2025-08-30 21:44:11 +0200

    answer to coke

--- week2/coke/coke.py
+++ week2/coke/coke.py
@@ -5,10 +5,21 @@
 """
 
 def main():
-
+    due()
 
 
 def due():
-    return
 
-if __name__=="__main__"
+    am_due=50
+
+    while am_due != 0:
+        if am_due >=0:
+            show=print("Amount Due: "+str(am_due))
+            pay=(input("Insert coin: "))
+            am_due=int(am_due)-int(pay)
+        else:
+            Change=print("Change Owed: " + str(am_due))
+            break
+
+if __name__=="__main__":
+    main()
