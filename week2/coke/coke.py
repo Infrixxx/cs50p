c73301fdd6841249c90ac5892ed5a457622bf24c
Boitumelo <bcrachoshi@gmail.com>
2025-08-30 22:26:28 +0200

    uppdate

--- week2/coke/coke.py
+++ week2/coke/coke.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 """
+Coke is 50 cents
 
+only accepts coins in these denominations: 25 cents, 10 cents, and 5 cents.
+
+if it's not accepted denomination, ignore
 """
 
 def main():
@@ -12,13 +16,19 @@ def due():
 
     am_due=50
 
-    while am_due != 0:
-        if am_due >=0:
+    while am_due >= 0:
+        if am_due > 0:
             show=print("Amount Due: "+str(am_due))
-            pay=(input("Insert coin: "))
-            am_due=int(am_due)-int(pay)
-        else:
-            Change=print("Change Owed: " + str(am_due))
+            pay=int((input("Insert coin: ")))
+            if ( pay == 25 or pay == 10 or pay == 5):
+                am_due=int(am_due)-int(pay)
+            else:
+                continue
+        elif am_due==0:
+            print("Change Owed: " + str(am_due))
+            break
+        elif am_due < 0:
+            Change=print("Change Owed: " + str(int(am_due)*-1))
             break
 
 if __name__=="__main__":
