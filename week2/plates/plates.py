b0ee1b5c236852e0461bb0549a2cb644e226e898
Boitumelo <bcrachoshi@gmail.com>
2025-08-31 03:51:21 +0200

    fix to files

--- week2/plates/plates.py
+++ week2/plates/plates.py
@@ -17,14 +17,28 @@ def main():
 
 
 def is_valid(s):
-    for i in plate:
-        if i=>"A" and i<="Z":
-            count+=1
-            if count<=6:
-                continue
-            else:
-                break
-    return
 
+    #check length of s
+    limit=len(s)
+    if limit <2 or limit >6:
+        return false
+    
+    #if all characters are alpha numerici
+        for i in s:
+            if not char.isalnum():
+            return False
 
-main()
+    # Check numbers are only at the end and first number isn't '0'
+    found_number = False
+    for char in s:
+        if char.isdigit():
+            if not found_number and char == '0':
+                return False  # First number can't be '0'
+            found_number = True
+        elif found_number:
+            return False  # Letter after number is invalid
+            
+    return True
+
+if __name__=="__main__":
+    main()
