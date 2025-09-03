2c2267c2da4c89385580895cf4ddae2052cafdb5
Boitumelo <bcrachoshi@gmail.com>
2025-09-03 16:56:46 +0200

    final touch

--- week2/nutrition/nutrition.py
+++ week2/nutrition/nutrition.py
@@ -14,35 +14,43 @@ pseudo code :
                 --->return false annd exit.
 """
 
-def main()
+def main():
     calories()
 
 def calories():
 
 
-fruit_calories = {
-    "apple": 130,
-    "avocado": 50,
-    "banana": 110,
-    "cantaloupe": 50,
-    "grapefruit": 60,
-    "grapes": 90,
-    "honeydew melon": 50,
-    "kiwifruit": 90,
-    "lemon": 15,
-    "lime": 20,
-    "nectarine": 60,
-    "orange": 80,
-    "peach": 60,
-    "pear": 100,
-    "pineapple": 50,
-    "plums": 70,
-    "strawberries": 50,
-    "sweet cherries": 100,
-    "tangerine": 50,
-    "watermelon": 80
-}
+    fruit_calories = {
+        "apple": 130,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 90,
+        "honeydew melon": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
+        }
 
 
 
     fruit=(input("Item: "))
+
+    if fruit not in fruit_calories:
+        return False
+    else:
+        return print(f"Calories : {fruit.title()}":{fruit_calories[fruit]})
+
+if __"name"__=="__main__":
+    main()
