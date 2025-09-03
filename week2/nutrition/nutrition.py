6c01ab30120d2d3062f9594c44305a26518a0b5a
Boitumelo <bcrachoshi@gmail.com>
2025-09-03 16:45:49 +0200

    dictionary

---
+++ week2/nutrition/nutrition.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+pseudo code :
+
+            --->Ask user to input a fruit
+            --->strip white spaces from input and make it lowercase.
+            --->Assume the user inputs fruit names written the same as in the poster.
+            --->make a dictionary of fruits and equate them to key value being the calories
+            --->check if the input exists in the said list of fruits
+            ---> if it exists, return the calories of the fruit.
+                --->return calories
+            --->if it doesnt exist, ignore the input.
+                --->return false annd exit.
+"""
+
+def main()
+    calories()
+
+def calories():
+
+
+    fruits={
+            "apple":"11",
+            'avocado':'50',
+            'banana':'110',
+            'cantalope':'50',
+            'grapefruit':'60',
+            'grapes':'90',
+
+            }
+
+
+
+    fruit=(input("Item: "))
