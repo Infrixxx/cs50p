5586e8011b3325b09ec94d42de96e5eeb88fceb1
Boitumelo <bcrachoshi@gmail.com>
2025-09-04 22:16:06 +0200

    eof block

---
+++ week3/taqueria/taqueria.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+a program that enables a user to place an order
+prompting them for items
+one per line
+until the user inputs control-d
+
+After each inputted item, display the total cost of all items inputted
+prefixed with a dollar sign ($) and formatted to two decimal places.
+Treat the user’s input case insensitively.
+
+Assume that every item on the menu will be titlecased
+"""
+
+def main():
+    return total()
+
+def total():
+
+    menu={
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+    }
+
+    total=0
+    
+    while True:
+        food=(input()).strip().lower().title()
+
+        if food in menu:
+            total+=menu[food]
+        else:
+            continue
+    return print(total)
+
+if __name__=="__main__":
+    main()
