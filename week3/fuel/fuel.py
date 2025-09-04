ed7dc1d21ac1eaa331bf109fcf0f10b19ca78629
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-04 21:06:39 +0200

    try and except

--- week3/fuel/fuel.py
+++ week3/fuel/fuel.py
@@ -12,22 +12,33 @@ if >= 99% return F for full
 
 if <=1% return E for empty
 
+
+Exceptions to take into account:
+
+            # - Non-integer input (ValueError)
+            # - Division by zero (ZeroDivisionError)  
+            # - Not enough values after split (IndexErro
+
 """
 
 def main():
     return fraction()
 
 def fraction():
-    number=(input("Fraction: ")).strip().split("/")
-
-    if number[0].isnumeric and number[1].isnumeric:
-        if int(number[0])>=0 and int(number[1])>=0:
-            x=int(number[0])
-            y=int(number[1])
-
-            return print(x,y)
-
-
+    while True:
+        try:
+            number=(input("Fraction: ")).strip().split("/")
+        
+            if number[0].isnumeric and number[1].isnumeric:
+                if int(number[0])>=0 and int(number[1])>=0:
+                    x=int(number[0])
+                    y=int(number[1])
+        
+                    return print(x,y)
+                    
+        except(ValueError, ZeroDivisionError, IndexError):
+                continue        
+        
 
 if __name__=="__main__":
     main()
