5d08afa8872dbc68ba2b7909997e35a5dce529ce
Boitumelo <bcrachoshi@gmail.com>
2025-09-05 01:02:12 +0200

    file structure

---
+++ week3/outdated/outdated.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+I need to take a date in 
+
+month/day/year and set it to YYYY-MM-DD
+
+So it can be in format of MM/DD/YYYY
+or
+Month_as_word DD, YYYY 
+"""
+
+def main():
+    return date()
+
+def date():
+    return print(date)
+
+if __name__=="__main__":
+    main()
