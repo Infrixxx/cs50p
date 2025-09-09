45ce764534128e138c6135d5e673a01b6f3bc82b
Boitumelo <bcrachoshi@gmail.com>
2025-09-10 01:50:04 +0200

    skeleton

---
+++ week4/adieu/adieu.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Prompt user for name
+Repeat until user terminates with ctrl-d
+"""
+
+def main():
+    return adieu()
+
+def adieu():
+    return
+
+if __name__=="__main__":
+    main()
+
