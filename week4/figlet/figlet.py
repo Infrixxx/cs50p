965289da4a397e5e5bc3e6bb91b8867504d9aa37
Boitumelo <bcrachoshi@gmail.com>
2025-09-09 18:24:14 +0200

    add to list

--- week4/figlet/figlet.py
+++ week4/figlet/figlet.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+import sys
 from pyfiglet import Figlet
 
+
+#to take the arguments from the system:
+
+print(f"number of arguments is {len(sys.argv)}")
+
 figlet=Figlet()
+
+if sys.argv[1]=="-f" or sys.argv[1]=="--font":
+    font_name=sys.argv[2]
+
+if font_name in figlet.getFonts():
+    figlet.setFont(font=f)
+
 input_str=input("Input: ")
-print(f"Output: {figlet.renderText(input_str)})
+print(f"Output: {figlet.renderText(input_str)}")
