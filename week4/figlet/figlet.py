9d92bbf78b25c3b82bdae97af48f508d3ccdd730
Boitumelo <bcrachoshi@gmail.com>
2025-09-09 18:48:26 +0200

    set font

--- week4/figlet/figlet.py
+++ week4/figlet/figlet.py
@@ -12,10 +12,15 @@ if len(sys.argv) == 3:
         font_name=sys.argv[2]
     
         if font_name in figlet.getFonts():
-            figlet.setFont(font=f)
+            figlet.setFont(font=font_name)
         else:
             print("Invalid font")
             sys.exit(1)
+    else:
+        print("Usage : figlet.py [--f FONT_NAME]")
+elif len(sys.argv)>1:
+    print("Usage : figlet.py [--f FONT_NAME]")
+    sys.exit(1)
 
 input_str=input("Input: ")
 print(f"Output: {figlet.renderText(input_str)}")
