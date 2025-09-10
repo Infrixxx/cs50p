a8be358098af9310abdae0de85fa2b314c4cbb93
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-10 02:15:22 +0200

    moved code out of except

--- week4/adieu/adieu.py
+++ week4/adieu/adieu.py
@@ -22,20 +22,21 @@ def adieu():
             name=input("Name: ").strip()
             names.append(name)
         except(EOFError):
-            fin_indx=len(names)-1
+            break
+        fin_indx=len(names)-1
 
-            for name in names:
-                if fin_indx > 1:
-                    if name!= names[fin_indx]:
-                        output_str+=name+", "
-                    else:
-                        output_str+="and "+name
-                elif fin_indx==1:
-                    output_str=names[0]+" and "+names[1]
+        for name in names:
+            if fin_indx > 1:
+                if name!= names[fin_indx]:
+                    output_str+=name+", "
                 else:
-                    output_str=name
-        return print(f"Adieu, adieu, to {output_str}")
-        break
+                    output_str+="and "+name
+            elif fin_indx==1:
+                output_str=names[0]+" and "+names[1]
+            else:
+                output_str=name
+    return print(f"Adieu, adieu, to {output_str}")
+
 if __name__=="__main__":
     main()
 
