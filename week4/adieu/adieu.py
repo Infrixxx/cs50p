fd21fca328675ffbca9054799c322687a19cdb46
Boitumelo <bcrachoshi@gmail.com>
2025-09-10 02:45:43 +0200

    comma

--- week4/adieu/adieu.py
+++ week4/adieu/adieu.py
@@ -26,7 +26,7 @@ def adieu():
         fin_indx=len(names)-1
 
     if fin_indx > 1:
-        output_str=", ".join(names[:-1]) + f" and {names[-1]}"
+        output_str=", ".join(names[:-1]) + f", and {names[-1]}"
     elif fin_indx==1:
         output_str=f"{names[0]} and {names[1]}"
     else:
