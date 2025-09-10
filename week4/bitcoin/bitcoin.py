017ba4b4c4971b1fc2060cdd9a9a103b7188b7c1
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-11 01:16:11 +0200

    request current price

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -8,13 +8,22 @@ if no argument sys.exit("Missing command-line argument")
 import requests
 import sys
 
+def main():
+    arguments_check()
+    
+def arguments_check():
+    if len(sys.argv) == 1:
+        sys.exit("Missing command-line argument")
+    
+    if not bitcoin_amount.isnumeric():
+            sys.exit("Command-line argument is not a number")
+    return bitcoin_amount=sys.argv[1]
 
-if len(sys.argv) == 1:
-    sys.exit("Missing command-line argument")
-
-bitcoin_amount=sys.argv[1]
-
-if not bitcoin_amount.isnumeric():
-        sys.exit("Command-line argument is not a number")
-
-response = requests.get('https://api.example.com/data')
+def get_current_price():
+    try:
+        url="https://api.coinbase.com/v2/prices/BTC-USD/spot"
+    
+        response = requests.get(url)
+        
+    except requests.RequestException:
+        continue
