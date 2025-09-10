eafc9bcea7a69beb5e0446f436eb033ac7fff277
Boitumelo <bcrachoshi@gmail.com>
2025-09-11 00:48:56 +0200

    bitcoin

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -4,13 +4,13 @@
 import requests
 import sys
 
-"""
-if len(sys.argv) > 1 :
-    if sys.arg[1].isnumeric:
+if len(sys.argv) > 2 :
+    if not sys.arg[1].isnumeric:
+        sys.exit("Command-line argument is not a number")
 
-    else:
-        print("Command-line argument is not a number")
 else:
     print("Missing command-line argument")
+
+
 """
 print(f"the number of arrguments is {len(sys.argv)}")
