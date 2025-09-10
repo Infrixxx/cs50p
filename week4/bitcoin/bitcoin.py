4c0a17759f901adf756f99351cbf0e92fdf9bfa9
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-11 00:57:57 +0200

    condition

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 """
+request user bitcoin ammount from command line
+check if it can be converted to float
+if not sys.exit("Command-line argument is not a number")
+if no argument sys.exit("Missing command-line argument")
 """
 import requests
 import sys
 
-if len(sys.argv) > 2 :
-    if not sys.arg[1].isnumeric:
-        sys.exit("Command-line argument is not a number")
 
-else:
-    print("Missing command-line argument")
+if len(sys.argv) == 1:
+    sys.exit("Missing command-line argument")
+
+bitcoin_amount=argv[1]
+
+if len(sys.argv) > 2 :
+    if not bitcoin_amount.isnumeric:
+    sys.exit("Command-line argument is not a number")
+
+
 
 
 """
