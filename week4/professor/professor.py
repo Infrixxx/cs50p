9a8edc69907e06a476c639be734e362418ab9256
Boitumelo <bcrachoshi@gmail.com>
2025-09-10 16:46:32 +0200

    unindent

--- week4/professor/professor.py
+++ week4/professor/professor.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import random
+
 """
 Calculator game:
     generates random equations
@@ -6,11 +8,26 @@ Calculator game:
     If the user enters incorrect input it returns "EEE"
     After 3 tries, it returns the correct answer.
 
+    prompts the user for level between 1-3, inclusive. (return value error if neither)
+    
+    x + y format
+    n=level is the number of digit
+    return user score at end, number of correct answers
 """
 
 def main():
-    
-def professor():
+
+def get_level():
+    while True:
+        try:
+            level=input("Level: ")
+            if level.isnumeric() and int(level) > 1
+                level=int(level)
+
+                if level == 1 or level == 2 level:
+
+        except ValueError:
+             continue
 
 
 if __name__=="__main__":
