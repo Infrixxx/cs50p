9f8bb64c0a4cdf97f88f980459a1ed262495da32
Boitumelo <bcrachoshi@gmail.com>
2025-09-10 16:28:09 +0200

    templat

---
+++ week4/professor/professor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Calculator game:
+    generates random equations
+    Waits for input from user
+    If the user enters incorrect input it returns "EEE"
+    After 3 tries, it returns the correct answer.
+
+"""
+
+def main():
+    
+def professor():
+
+
+if __name__=="__main__":
+    main()
