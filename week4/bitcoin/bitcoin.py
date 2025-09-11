fb3a2e4bfeb005c26571b8b7ef72e51a718ef06f
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-11 02:04:03 +0200

    Perfect

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -2,11 +2,11 @@
 import requests
 import sys
 
-API_KEY =  ecd96072043d77cc2fe0bfa80636b639ff2e2795a62c49cf4f1d1931b2d139b3 # Replace with your real CoinCap API key
+API_KEY =  "ecd96072043d77cc2fe0bfa80636b639ff2e2795a62c49cf4f1d1931b2d139b3" #CoinCap API key
 url = f"https://rest.coincap.io/v3/assets/bitcoin?apiKey={API_KEY}"
 
 def main():
-    bitcoin_amount = get_arguments()
+    bitcoin_amount = arguments_check()
     price = get_current_price()
     cost = bitcoin_amount * price
     print(f"${cost:,.4f}")  
@@ -15,8 +15,8 @@ def arguments_check():
     if len(sys.argv) < 2:
         sys.exit("Missing command-line argument")
     
-    try:
-        # Try to convert to float
+    try:# Try to convert to float
+        
         bitcoin_amount = float(sys.argv[1])
         return bitcoin_amount
         
@@ -25,7 +25,7 @@ def arguments_check():
 
 def get_current_price():
     try:
-    
+        #fetching data from coincap
         response = requests.get(url)
         response.raise_for_status()  # catch HTTP errors
         res = response.json() 
