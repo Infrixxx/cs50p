66a63b2cba699420dbb9a2e520347e4728842f62
Boitumelo <bcrachoshi@gmail.com>
2025-09-12 01:40:40 +0200

    test cases

--- week5/test_twttr/test_twttr.py
+++ week5/test_twttr/test_twttr.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 
+from twttr import shorten
 
+def test_shorten():
+    assert shorten("aeiou") == ""
+    assert shorten("AEIOU") == ""
+    assert shorten("Who are you?") == "Wh r y?"
--- week5/test_twttr/twttr.py
+++ week5/test_twttr/twttr.py
@@ -9,15 +9,15 @@ def main():
     vowel_remover()
 
 def shorten(word):
-    
-    word=word.lower().strip()
 
-    vowels={"a","e","i","o","u"}
-
+    vowels_l={"a","e","i","o","u"}
+    vowels_u={"A","E","I","O","U"}
     final_word=""
 
     for i in word:
-        if i.lower() in vowels:
+        if i in vowels_l:
+            continue
+        elif i in vowels_u:
             continue
         else:
             final_word+=i
