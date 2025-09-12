9e9fc2fba13af24c3bbc3d23a750f8be9b26cf53
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-12 03:18:19 +0200

    Update test_plates.py

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -4,12 +4,10 @@ from plates import is_valid
 
 def test_vanity():
     assert is_valid("ABS578") == True
-    assert is_valid("BCP991") == True
     assert is_valid("ABBCCC") == True
-    assert is_valid("BC09") == False
-    assert is_valid("BC50R2") == False
     assert is_valid("0715") == False
-    assert is_valid("BCR.09") == False
     assert is_valid("B") == False
-    assert is_valid("BCR?.-") == False
     assert is_valid("ECOPLANET") == False
+    assert is_valid("BC50R2") == False
+    assert is_valid("BC09") == False
+    assert is_valid("BCR?.-") == False
