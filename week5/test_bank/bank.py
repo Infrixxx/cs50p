23a30e89d856d393af656fa9403606f1c0039d6a
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-12 02:05:42 +0200

    Update bank.py

--- week5/test_bank/bank.py
+++ week5/test_bank/bank.py
@@ -7,12 +7,14 @@ def main():
 
 def value(greeting)
 
-    if greeting.startswith("hello"):
-        print("$0")
-    elif s.startswith("h"):
-        print("$20")
-    else:
-        print("$100")
+greeting=greeting.lower().strip()
+
+if greeting[0:5]=="hello":
+    print("$0")
+elif greeting[0]=="h":
+    print("$20")
+else:
+    print("$100")
 
 if __name__="__main__":
 main()
