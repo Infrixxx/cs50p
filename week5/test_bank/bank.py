c5edbe1237d45ac0f992d87d941a06bdd7d1d12c
Boitumelo <bcrachoshi@gmail.com>
2025-09-12 02:03:14 +0200

    test cases and moving

---
+++ week5/test_bank/bank.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def main():
+    greeting = input("Greeting: ").strip().lower()
+    value(greeting)
+
+
+def value(greeting)
+
+    if greeting.startswith("hello"):
+        print("$0")
+    elif s.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+if __name__="__main__":
+main()
---
+++ week5/test_bank/test_bank.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from bank import value
+
+
+def test_value():
+    assert value("Hello") == "$0"
+    assert value("Hy") == "$20"
+    assert value("Dumela") == "$100"
