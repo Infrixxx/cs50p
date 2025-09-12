713c3834ae6b69e34b54e102ed3536a6aa9e7682
Boitumelo <bcrachoshi@gmail.com>
2025-09-12 02:15:51 +0200

    return value fix

--- week5/test_bank/bank.py
+++ week5/test_bank/bank.py
@@ -9,11 +9,11 @@ def value(greeting):
     greeting=greeting.lower().strip()
 
     if greeting[0:5]=="hello":
-        print("$0")
+        return 0
     elif greeting[0]=="h":
-        print("$20")
+        return 20
     else:
-        print("$100")
+        return 100
 
 if __name__=="__main__":
     main()
--- week5/test_bank/test_bank.py
+++ week5/test_bank/test_bank.py
@@ -3,6 +3,8 @@
 from bank import value
 
 def test_value():
-    assert value("Hello") == "$0"
-    assert value("Hy") == "$20"
-    assert value("Dumela") == "$100"
+    assert value("Hello") == 0
+    assert value("Hy") == 20
+    assert value("Dumela") == 100
+    assert value("123hello") == 0
+    assert value("!?.hello") == 0
