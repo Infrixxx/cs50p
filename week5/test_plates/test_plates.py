333005ccf911321d6dbfd92317ea88b3b2351071
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-12 03:13:05 +0200

    Update test_plates.py

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -6,9 +6,9 @@ def test_vanity():
     assert is_valid("ABS578") == True
     assert is_valid("BCP991") == True
     assert is_valid("ABBCCC") == True
-    assert is_valid("BC078B") == False
-    assert is_valid("BC071") == False
-    assert is_valid("071") == False
-    assert is_valid("!@#$%") == False
-    assert is_valid("tylo75489") == False
-    assert is_valid("BCR057") == False
+    assert is_valid("BC09") == False
+    assert is_valid("BC50R2") == False
+    assert is_valid("0715") == False
+    assert is_valid("BCR.09") == False
+    assert is_valid("B") == False
+    assert is_valid("BCR?.-") == False
