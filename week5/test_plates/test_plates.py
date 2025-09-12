db4b5f0adc2e4ed507dd0e9683725a04de2b4f4e
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-12 03:30:59 +0200

    FIX TO TEST CASES

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -3,13 +3,13 @@
 from plates import is_valid
 
 def test_vanity():
-    assert is_valid("AB") == True
-    assert is_valid("XY7890") == True
-    assert is_valid("ZZZ999") == True
-    assert is_valid("12BB") == False
-    assert is_valid("B0CCAR") == False
-    assert is_valid("BB078") == False
-    assert is_valid("--AB12") == False
-    assert is_valid("A") == False
+    assert is_valid("AB50") == True
+    assert is_valid("BCRA90") == True
+    assert is_valid("HEAVEN") == True
+    assert is_valid("HEA05") == False
+    assert is_valid("HEA76B") == False
+    assert is_valid("BB07C8") == False
+    assert is_valid("123456") == False
+    assert is_valid("ABC.PI") == False
     assert is_valid("LONGPLATE") == False
-    assert is_valid("ABC-123") == False
+    assert is_valid("ABC-.*") == False
