621f4d4f3fe4811db4ba6fd3cbfdfe014a959e51
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2025-09-12 03:25:43 +0200

    Update test_plates.py

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -6,10 +6,10 @@ def test_vanity():
     assert is_valid("AB123") == True
     assert is_valid("XY7890") == True
     assert is_valid("ZZZ999") == True
-    assert is_valid("A1B2") == False
+    assert is_valid("12BB") == False
     assert is_valid("B0C") == False
-    assert is_valid("9ABC") == False
-    assert is_valid("!AB123") == False
+    assert is_valid("BB078") == False
+    assert is_valid("--AB12") == False
     assert is_valid("A") == False
     assert is_valid("LONGPLATE") == False
     assert is_valid("ABC-123") == False
