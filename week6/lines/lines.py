77748ffd412f0bada06a36746dada9c4264d48d3
Boitumelo <bcrachoshi@gmail.com>
2025-09-14 02:22:01 +0200

    fix:

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -6,14 +6,4 @@ Check if it's a valid filename.
 """
 
 if len(sys.argv) != 2 or not sys.argv[1].endswith(".py"):
-    sys.exit("maaka.")
-
-try:
-    with open(sys.argv[1]) as file:
-        print(
-            sum(
-                1 for line in file if line.strip() and not line.lstrip().startswith("#")
-            )
-        )
-except FileNotFoundError:
-    sys.exit("File does not exist.")
+    sys.exit("Invalid argument.")
