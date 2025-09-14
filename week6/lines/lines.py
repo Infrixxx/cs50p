ffa2b3fae015f27d15d800a0b3c0bafdc35bc84a
Boitumelo <bcrachoshi@gmail.com>
2025-09-14 02:17:54 +0200

    push

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -6,4 +6,14 @@ Check if it's a valid filename.
 """
 
 if len(sys.argv) != 2 or not sys.argv[1].endswith(".py"):
-    sys.exit("")
+    sys.exit("Invalid arguments.")
+
+try:
+    with open(sys.argv[1]) as file:
+        print(
+            sum(
+                1 for line in file if line.strip() and not line.lstrip().startswith("#")
+            )
+        )
+except FileNotFoundError:
+    sys.exit("File does not exist.")
