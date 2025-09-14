e57dadbf1bd903e5da1baa20934779dac8688bc3
Boitumelo <bcrachoshi@gmail.com>
2025-09-14 02:58:03 +0200

    template

---
+++ week6/pizza/pizza.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Expects one command line argument ( name of a csv file or path)
+Outputs table formatted as Ascii art using tabulate
+Format table using grid format.
+
+conditions:
+    if the user doesn't specify one command line argument or if the specified filename doesnt end with      csv, or the file doesn't not exist, program should exit via sys.exit
+"""
+
+import sys
+import csv
+import tabulate
+
+if len(sys.argv) < 2 :
+    sys.exit("Too few command-line arguments")
+
+if len(sys.argv) > 2 :
+    sys.exit("Too many command-line arguments")
+
+if not sys.argv[1].endswith(".csv"):
+    sys.exit("Not a CSV file")
+try:
+    with open(sys.argv[1],r ) as file:
+        lines=file.readline()
+except FileNotFoundError:
+    sys.exit("File Does not exist")
