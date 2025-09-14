f0d2ee3d60bcad32eb365d54c570f7702c2b3e33
Boitumelo <bcrachoshi@gmail.com>
2025-09-14 02:14:54 +0200

    fix

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -6,10 +6,4 @@ Check if it's a valid filename.
 """
 
 if len(sys.argv) != 2 or not sys.argv[1].endswith(".py"):
-    sys.exit("Invalid arguments")
-
-try:
-    with open(sys.argv[1]) as f:
-        print(sum( 1 for line in f if line.strip() and not line.Istrip().startswith("#")))
-except FileNotFoundError:
-    sys.exit("File does not exist.")
+    sys.exit("")
