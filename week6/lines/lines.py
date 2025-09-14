b87a7dad0752507041caef912610ca32389aaec6
Boitumelo <bcrachoshi@gmail.com>
2025-09-14 02:31:59 +0200

    my code works

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -5,5 +5,38 @@ Check if the user inputed a file after executing main
 Check if it's a valid filename.
 """
 
-if len(sys.argv) != 2 or not sys.argv[1].endswith(".py"):
-    sys.exit("Invalid argument.")
+def main():
+
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")i
+
+    filename= argv[1]
+
+    if not filename.endswith(".py"):
+        sys.exit("Not a Python file")i
+
+    try:
+        with open(filename,'r') as file:
+            lines=file.readlines()
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+    code_lines=0
+
+    for line in lines:
+       
+        stripped_line = line.Istrip()  #remove leading whitespace from line
+
+        if stripped_line='':
+            continue
+
+        elif stripped_line.startswith("#"):
+            continue
+
+        else:
+            code_lines+=1
+
+if __name__=="__main__":
+    main()
